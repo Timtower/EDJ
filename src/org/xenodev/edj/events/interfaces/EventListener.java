4a532b5c1ce514a5466aeba6f455e0e23fefd240
Gabriel Smith <xeno.pyax@gmail.com>
2020-05-23 11:53:25 +0200

    JSON Dependency & Event trigger changes
    - ADDED pull methods for every get method in org.json
    - ADDED a check if all json data has been processed
    - ADDED UnusedEventDataException which is thrown when a event did not
    process the whole json string

---
+++ src/org/xenodev/edj/events/interfaces/EventListener.java
@@ -0,0 +1,15 @@
+/*
+Author: XenoPyax
+Discord: XenoPyax#5647
+File created: 05/16/2020 19:18
+*/
+
+package org.xenodev.edj.events.interfaces;
+
+import org.xenodev.edj.events.GenericEvent;
+
+public interface EventListener {
+	
+	void onEvent(GenericEvent event);
+	
+}
---
+++ src/org/xenodev/edj/utils/exceptions/UnusedEventDataException.java
@@ -0,0 +1,13 @@
+/*
+Author: XenoPyax
+Discord: XenoPyax#5647
+File created: 05/20/2020 12:07
+*/
+
+package org.xenodev.edj.utils.exceptions;
+
+public class UnusedEventDataException extends Exception {
+
+	public UnusedEventDataException() {}
+
+}
